c5fe0f5491d2270065904a27d8612c854d48c397
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2023-03-04 08:13:37 +0800

    Use drop down to go to another page

--- scrapers/page_scraper.py
+++ scrapers/page_scraper.py
@@ -1,6 +1,8 @@
 import logging
 import json
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 from drivers import driver
 
@@ -9,12 +11,20 @@ logger = logging.getLogger("lihkg-scraper")
 
 
 def scrape_page(thread_id, page_number, open_new_tab=False):
-    if open_new_tab:
-        driver.switch_to.new_window("tab")
-
-    page_url = f"https://lihkg.com/thread/{thread_id}/page/{page_number}"
+    thread_url = f"https://lihkg.com/thread/{thread_id}"
+    page_url = f"{thread_url}/page/{page_number}"
     logger.debug(f"Scraping {page_url}")
-    driver.get(page_url)
+
+    if driver.current_url.startswith(thread_url) and page_number % 7 != 0:
+        # Navigating to a different page using the drop down list prevents triggering of Cloudflare Turnstile
+        # The page is still reloaded when `page_number % 7 != 0` to reduce RAM usage
+        page_select = Select(driver.find_elements(By.TAG_NAME, "select")[-2])
+        page_select.select_by_value(str(page_number))
+    else:
+        if open_new_tab:
+            driver.switch_to.new_window("tab")
+
+        driver.get(page_url)
 
     performance_logs_filtered = []
     TIMEOUT = 5
