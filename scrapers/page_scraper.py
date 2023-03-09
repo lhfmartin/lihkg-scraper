afb06a2183d7b302007d4805999129545dc90838
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2023-03-10 00:24:44 +0800

    Add randomness to dom reuse limit

--- scrapers/page_scraper.py
+++ scrapers/page_scraper.py
@@ -1,6 +1,7 @@
 import logging
 import json
 import time
+from random import randint
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
@@ -18,9 +19,13 @@ def scrape_page(thread_id, page_number, open_new_tab=False):
     page_url = f"{thread_url}/page/{page_number}"
     logger.debug(f"Scraping {page_url}")
 
-    if driver.current_url.startswith(thread_url) and scrape_page.dom_reuse_count < 6:
+    if driver.current_url.startswith(
+        thread_url
+    ) and scrape_page.dom_reuse_count < randint(
+        max(6, scrape_page.dom_reuse_count), 10
+    ):
         # Navigating to a different page using the drop down list prevents triggering of Cloudflare Turnstile
-        # The page is still reloaded when the current DOM is reused 6 times (i.e. 7 pages have been rendered) to reduce RAM usage
+        # The page is still reloaded when the current DOM is reused 6 - 10 times (i.e. 7 - 11 pages have been rendered) to reduce RAM usage
         page_select = Select(driver.find_elements(By.TAG_NAME, "select")[-2])
         page_select.select_by_value(str(page_number))
         scrape_page.dom_reuse_count += 1
