e6b09e89e531c90563a57fbddc6a254a3c5f29b1
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2023-10-07 01:49:29 +0800

    Allow specifying executable_path in config

--- drivers/chrome_driver.py
+++ drivers/chrome_driver.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.service import Service
 
 from config import config
 
@@ -9,4 +9,9 @@ opt.add_experimental_option(
     "debuggerAddress", config["chrome_driver"]["debugger_address"]
 )
 opt.set_capability("goog:loggingPrefs", {"performance": "ALL"})
-chrome_driver = webdriver.Chrome(options=opt)
+
+service = None
+if len(config["chrome_driver"]["executable_path"]) > 0:
+    service = Service(executable_path=config["chrome_driver"]["executable_path"])
+
+chrome_driver = webdriver.Chrome(options=opt, service=service)
