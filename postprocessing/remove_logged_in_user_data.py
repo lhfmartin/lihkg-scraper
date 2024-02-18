dc244cd0dd10a6e09e4e70adb169d5c980d1b2ba
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2024-02-18 10:20:53 +0800

    Remove logged in user data in messages.json

--- postprocessing/remove_logged_in_user_data.py
+++ postprocessing/remove_logged_in_user_data.py
@@ -1,4 +1,4 @@
-def remove_logged_in_user_data_from_thread_data(thread_data):
+def _remove_logged_in_user_data_from_thread_data(thread_data):
     thread_data.pop("me", None)
     thread_data.pop("vote_status", None)
     thread_data.pop("is_bookmarked", None)
@@ -6,26 +6,39 @@ def remove_logged_in_user_data_from_thread_data(thread_data):
     thread_data.pop("last_read", None)
     thread_data.pop("last_replied", None)
     thread_data.get("pinned_post", {}).pop("vote_status", None)
+    if "parent_thread" in thread_data:
+        _remove_logged_in_user_data_from_thread_data(thread_data["parent_thread"])
+    for x in thread_data.get("child_threads", []):
+        _remove_logged_in_user_data_from_thread_data(x)
 
 
-def remove_logged_in_user_data_from_page_data(page_data):
-    page_data["response"].pop("vote_status", None)
-    messages = page_data["response"]["item_data"]
+def _remove_logged_in_user_data_from_messages(messages):
     for message in messages:
         while message.pop("vote_status", None) is not None:
             message = message.get("quote", {})
 
 
+def _remove_logged_in_user_data_from_page_data(page_data):
+    _remove_logged_in_user_data_from_thread_data(page_data["response"])
+
+    messages = page_data["response"]["item_data"]
+    _remove_logged_in_user_data_from_messages(messages)
+
+
 def remove_logged_in_user_data(thread_dao, page_dao):
     topic = thread_dao.load_topic()
-    remove_logged_in_user_data_from_thread_data(topic)
-    remove_logged_in_user_data_from_thread_data(topic.get("parent_thread", {}))
-    for x in topic.get("child_threads", []):
-        remove_logged_in_user_data_from_thread_data(x)
+    _remove_logged_in_user_data_from_thread_data(topic)
     thread_dao.save_topic(topic)
 
     page_numbers = page_dao.get_available_page_numbers()
     for page_number in page_numbers:
         page_data = page_dao.load_page(page_number)
-        remove_logged_in_user_data_from_page_data(page_data)
+        _remove_logged_in_user_data_from_page_data(page_data)
         page_dao.save_page(page_number, page_data)
+
+    try:
+        messages = thread_dao.load_messages()
+        _remove_logged_in_user_data_from_messages(messages)
+        thread_dao.save_messages(messages)
+    except FileNotFoundError:
+        pass
