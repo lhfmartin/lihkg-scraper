9117f31e7a02b7a8bdc22de4eb50c7cb75419666
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2024-11-04 23:48:48 +0900

    Fix bug "No data found for resource with given identifier"

--- scrapers/utils/listen_network_responses.py
+++ scrapers/utils/listen_network_responses.py
@@ -18,33 +18,35 @@ def _url_matches_patterns(url: str, patterns: list[str]) -> bool:
 def listen_network_responses(
     url_patterns: list[str],
 ) -> tuple[str, str | int | float | dict | list]:
-    performance_logs_filtered = []
+    req_id = None
+    res_url = None
+    is_res_completely_loaded = False
 
     t_0 = time.perf_counter()
-    while len(performance_logs_filtered) < 1:
+    while not is_res_completely_loaded:
         if time.perf_counter() - t_0 > LOADING_TIMEOUT:
-            logger.error(f"Timeout exceeded")
-            break
+            raise TimeoutError("Timeout exceeded")
         performance_logs = driver.get_log("performance")
         performance_logs = [
             json.loads(pl["message"])["message"] for pl in performance_logs
         ]
 
-        performance_logs = [
-            pl
-            for pl in performance_logs
-            if pl["method"] == "Network.responseReceived"
-            and "json" in pl["params"]["response"]["mimeType"]
-            and _url_matches_patterns(pl["params"]["response"]["url"], url_patterns)
-        ]
+        for pl in performance_logs:
+            if (
+                pl["method"] == "Network.responseReceived"
+                and "json" in pl["params"]["response"]["mimeType"]
+                and _url_matches_patterns(pl["params"]["response"]["url"], url_patterns)
+            ):
+                req_id = pl["params"]["requestId"]
+                res_url = pl["params"]["response"]["url"]
+            elif (
+                pl["method"] == "Network.loadingFinished"
+                and pl["params"]["requestId"] == req_id
+            ):
+                is_res_completely_loaded = True
 
-        performance_logs_filtered += performance_logs
         time.sleep(0.1)
 
-    assert len(performance_logs_filtered) == 1
-
-    log = performance_logs_filtered[0]
-    req_id = log["params"]["requestId"]
     res = driver.execute_cdp_cmd("Network.getResponseBody", {"requestId": req_id})
 
-    return log["params"]["response"]["url"], json.loads(res["body"])
+    return res_url, json.loads(res["body"])
