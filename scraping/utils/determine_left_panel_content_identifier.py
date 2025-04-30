782f408696375ce7db1bd72f4fb51c8547dd6f52
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2025-04-29 22:02:31 -0300

    Fix bugs

--- scraping/utils/determine_left_panel_content_identifier.py
+++ scraping/utils/determine_left_panel_content_identifier.py
@@ -5,16 +5,16 @@ def determine_left_panel_content_identifier(res_url: str, res_body: dict) -> str
     left_panel_content_identifier = None
 
     if "lihkg.com/api_v2/thread/bookmark" in res_url:
-        left_panel_content_identifier = (
-            "bookmark_" + res_body["response"]["me"]["user_id"]
+        left_panel_content_identifier = "bookmark_" + str(
+            res_body["response"]["me"]["user_id"]
         )
     elif "lihkg.com/api_v2/thread/" in res_url and "cat_id=" in res_url:
         left_panel_content_identifier = "cat_" + re.search(
-            "cat_id=(\d+)", res_url
+            r"cat_id=(\d+)", res_url
         ).group(1)
     elif "lihkg.com/api_v2/user/" in res_url:
         left_panel_content_identifier = "user_" + re.search(
-            "lihkg.com/api_v2/user/(\d+)/thread?", res_url
+            r"lihkg\.com/api_v2/user/(\d+)/thread\?", res_url
         ).group(1)
 
     return left_panel_content_identifier
