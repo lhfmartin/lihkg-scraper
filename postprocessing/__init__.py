8c2e3102b0442441d79367baadf3ff87d502afdc
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2026-07-24 23:30:44 +0800

    Fix import order requirement

--- postprocessing/__init__.py
+++ postprocessing/__init__.py
@@ -1,7 +1,4 @@
 from .remove_logged_in_user_data import remove_logged_in_user_data
 from .consolidate_messages import consolidate_messages
 from .download_images import download_images
-
-from .apply import (
-    apply,
-)  # It should be imported last because it depends on the functions above
+from .apply import apply
--- postprocessing/apply.py
+++ postprocessing/apply.py
@@ -2,11 +2,9 @@ from dao import ImageDao, PageDao, ThreadDao, TopicListDao
 from enums import ArtifactCategory, PostProcessingActions
 import logging
 from models import ArtifactMetadata
-from postprocessing import (
-    remove_logged_in_user_data,
-    consolidate_messages,
-    download_images,
-)
+from .remove_logged_in_user_data import remove_logged_in_user_data
+from .consolidate_messages import consolidate_messages
+from .download_images import download_images
 
 logger = logging.getLogger("lihkg-scraper")
 
